99da68423ed8674858e55a96c1f5cc4f2f016102
Isaac Yonemoto <isaac.yonemoto@gmail.com>
2020-09-30 17:54:28 -0700

    add null integration tests for the line and label opcodes

---
+++ test/opcode/misc_test.exs
@@ -0,0 +1,41 @@
+defmodule MavisTest.Opcode.MiscTest do
+
+  # miscellaneous opcodes that don't do as much
+
+  use ExUnit.Case, async: true
+
+  @moduletag :opcodes
+
+  import Type, only: :macros
+  alias Type.{Function, Inference}
+
+  describe "funcinfo opcode" do
+    test "integration" do
+      code = [{:func_info, {:atom, :module}, {:atom, :func}, 2}]
+
+      assert {:ok, %Function{
+        params: [builtin(:any)], return: builtin(:any)
+      }} = Inference.run(code, %{0 => builtin(:any)})
+    end
+  end
+
+  describe "label opcode" do
+    test "integration" do
+      code = [label: 14]
+
+      assert {:ok, %Function{
+        params: [builtin(:any)], return: builtin(:any)
+      }} = Inference.run(code, %{0 => builtin(:any)})
+    end
+  end
+
+  describe "line opcode" do
+    test "integration" do
+      code = [line: 19]
+
+      assert {:ok, %Function{
+        params: [builtin(:any)], return: builtin(:any)
+      }} = Inference.run(code, %{0 => builtin(:any)})
+    end
+  end
+end
