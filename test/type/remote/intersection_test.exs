c6181431fd1bbe64ad09a1378cfb1fd97377fd19
Isaac Yonemoto <isaac.yonemoto@gmail.com>
2020-10-14 08:39:03 -0700

    setup tests for remote intersection

---
+++ test/type/remote/intersection_test.exs
@@ -0,0 +1,28 @@
+defmodule TypeTest.Remote.IntersectionTest do
+  use ExUnit.Case, async: true
+  use Type.Operators
+
+  @moduletag :intersection
+
+  import Type, only: :macros
+
+  alias Type.Bitstring
+
+  describe "the intersection of String.t" do
+    test "with itself and any is itself" do
+      assert remote(String.t()) == remote(String.t()) <~> remote(String.t())
+      assert remote(String.t()) == builtin(:any) <~> remote(String.t())
+    end
+
+    test "with empty bitstring is empty bitstring" do
+      assert @empty_bitstring == @empty_bitstring <~> remote(String.t())
+    end
+
+    test "with all other types is none" do
+      TypeTest.Targets.except([@empty_bitstring])
+      |> Enum.each(fn target ->
+        assert builtin(:none) == remote(String.t()) <~> target
+      end)
+    end
+  end
+end
