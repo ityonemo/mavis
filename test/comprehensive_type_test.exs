1e63a64efee476bd9be08240706a38674ebbd746
Isaac Yonemoto <isaac.yonemoto@gmail.com>
2020-10-15 17:07:28 -0700

    correctly parses all specs except those in File, IO, IO.ANSI Code, Macro

--- test/comprehensive_type_test.exs
+++ test/comprehensive_type_test.exs
@@ -7,20 +7,25 @@ defmodule TypeTest.ComprehensiveTypeTest do
 
   import Type, only: :macros
 
-  @elixir_modules Enum.map(~w(Kernel Kernel.SpecialForms Atom Base Bitwise Date
-  DateTime Exception Float Function Integer Module NaiveDateTime Record
+  @elixir_modules Enum.map(~w(Kernel Kernel.SpecialForms Atom Base Bitwise
+  Date DateTime Exception Float Function Integer Module NaiveDateTime Record
   Regex String Time Tuple URI Version Version.Requirement Access Date.Range
-  Enum Keyword List Map MapSet Range Stream File File.Stat File.Stream
-  IO.Stream OptionParser Path Port StringIO System Calendar Calendar.ISO
-  Calendar.TimeZoneDatabase Calendar.UTCOnlyTimeZoneDatabase Agent Application
-  Config Config.Provider Config.Reader DynamicSupervisor GenServer Node Process
-  Registry Supervisor Task Task.Supervisor Collectable Enumerable Inspect
-  Inspect.Algebra Inspect.Opts List.Chars Protocol String.Chars Code
-  Kernel.ParallelCompiler Macro.Env), &Module.concat(Elixir, &1))
+  Enum Keyword List Map MapSet Range Stream ~File File.Stat File.Stream
+  ~IO ~IO.ANSI IO.Stream OptionParser Path Port StringIO System Calendar
+  Calendar.ISO Calendar.TimeZoneDatabase Calendar.UTCOnlyTimeZoneDatabase
+  Agent Application Config Config.Provider Config.Reader DynamicSupervisor
+  GenServer Node Process Registry Supervisor Task Task.Supervisor Collectable
+  Enumerable Inspect Inspect.Algebra Inspect.Opts List.Chars Protocol
+  String.Chars ~Code Kernel.ParallelCompiler ~Macro Macro.Env),
+  &Module.concat(Elixir, &1))
 
 
   test "all types" do
-    Enum.each(@elixir_modules, fn module ->
+    @elixir_modules
+    |> Enum.filter(fn module ->
+      function_exported?(module, :__info__, 1)
+    end)
+    |> Enum.each(fn module ->
       case Code.Typespec.fetch_types(module) do
         {:ok, types} ->
           types
@@ -48,15 +53,16 @@ defmodule TypeTest.ComprehensiveTypeTest do
   end
 
   test "all specs" do
-    Enum.each(@elixir_modules, fn module ->
+    @elixir_modules
+    |> Enum.filter(fn module ->
+      function_exported?(module, :__info__, 1)
+    end)
+    |> Enum.each(fn module ->
       Enum.each(module.module_info[:exports], &do_fetch_spec(module, &1))
     end)
   end
 
   defp do_fetch_spec(module, {fun, arity}) do
-    module |> IO.inspect(label: "55")
-    fun |> IO.inspect(label: "56")
-    arity |> IO.inspect(label: "57")
     Type.fetch_spec(module, fun, arity)
   end
 
