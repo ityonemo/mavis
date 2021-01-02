fde83f6cd66007c629394ed9633bc41214972209
Isaac Yonemoto <isaac.yonemoto@gmail.com>
2021-01-02 14:22:45 -0800

    adds capability for literal atoms

--- lib/type/_properties.ex
+++ lib/type/_properties.ex
@@ -21,49 +21,6 @@ defprotocol Type.Properties do
   def normalize(type)
 end
 
-defimpl Type.Properties, for: Atom do
-  import Type, only: :macros
-
-  use Type.Helpers
-
-  alias Type.Message
-
-  group_compare do
-    def group_compare(_, atom()), do: :lt
-    def group_compare(left, right),       do: (if left >= right, do: :gt, else: :lt)
-  end
-
-  usable_as do
-    def usable_as(_, atom(), _), do: :ok
-    def usable_as(atom, node_type(), meta) do
-      if Type.Properties.Type.valid_node?(atom) do
-        :ok
-      else
-        {:error, Message.make(atom, node_type(), meta)}
-      end
-    end
-    def usable_as(atom, module(), meta) do
-      if Type.Properties.Type.valid_module?(atom) do
-        :ok
-      else
-        {:maybe, [Message.make(atom, module(), meta)]}
-      end
-    end
-  end
-
-  intersection do
-    def intersection(atom, atom()), do: atom
-    def intersection(atom, node_type()) do
-      if Type.Properties.Type.valid_node?(atom), do: atom, else: none()
-    end
-    def intersection(atom, module()) do
-      if Type.Properties.Type.valid_module?(atom), do: atom, else: none()
-    end
-  end
-
-  def subtype?(a, b), do: usable_as(a, b, []) == :ok
-end
-
 # remember, the empty list is its own type
 defimpl Type.Properties, for: List do
   import Type, only: :macros
